e0d759e121c04656f51297812b19e0dd21ae468e
ricardoquitian <leonquitian2000@gmail.com>
2023-08-08 15:06:08 -0500

    avance de la poke api

---
+++ POKE-API-PROTALENTO/api/controllers/controllers.js
@@ -0,0 +1,6 @@
+export function saludar () {
+    console.log ("Hello, my name is Luffy")
+
+}
+
+/*(export) se utiliza para indicarle a la funcion que està lista para ser exportada a otro archivo JS*/ 
---
+++ POKE-API-PROTALENTO/api/main.js
@@ -0,0 +1,5 @@
+import { saludar } from "./controllers/controllers.js";
+
+saludar ()
+
+/*(import), seguido del nombre de la función, y la ubicación del documento, nos permite arrastrar la información de otro documento para utilizarla*/
